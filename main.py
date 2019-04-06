2876ea70f28a7524ee2491e8464f07ddc5c87be0
standthis <standthis@protonmail.ch>
2019-04-06 16:52:06 +0200

    imagemagick broke

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('extra/mcq.pdf')
+
+
+cv.imshow("Black", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+#cv.cvtColor(image, cv2.COLOR_BGR2GRAY)
